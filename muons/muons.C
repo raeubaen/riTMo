ef1afecf86c589cf66466a040b17a48bdbd9c45a
ediociat <ele.dct@gmail.com>
2023-09-26 18:42:48 +0200

    .

--- muons/muons.C
+++ muons/muons.C
@@ -5,14 +5,16 @@ void muons(){
 
   auto *hbeta = new TH1F("hbeta", "hbeta", 10000, 0, 1);
   auto *hcs = new TH1F("hcs", "hcs", 10000, 0, 1000000);
-  auto *length12 = new TH2D("length12", "length12", 10, 0, 100, 10, 0, 100);
 
-  for(int i=0; i<10000000; i++){ double cme = gRandom->Gaus(211.4, 0.3); hbeta->Fill(beta(cme)); hcs->Fill(cs(beta(cme)));}
+  for(int i=0; i<100000; i++){
+    double cme = gRandom->Gaus(211.4, 0.3);
+    hbeta->Fill(beta(cme));
+    cout << cs(beta(cme)) << endl;
+    hcs->Fill(cs(cme));
+  }
 
-  for(int i=0; i<1000000000; i++){ double b = hbeta->GetRandom(); length12->Fill(gRandom->Exp(b/sqrt(1-b*b) * 2110 * 300), gRandom->Exp(b/sqrt(1-b*b) * 2110 * 300), 1);}
   hbeta->SaveAs("muonsbeta.root");
   hcs->SaveAs("muonscs.root");
-  length12->SaveAs("length12.root");
 }
 
 double michel_cutsuppression(){
