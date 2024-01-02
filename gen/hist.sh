7581187441eb97e9a2c9f9cf9c05b812a615a4da
ruben <r@g.it>
2024-01-02 14:50:51 +0100

    ..

--- gen/hist.sh
+++ gen/hist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-njob=$(expr $1 + 200)
+njob=$(expr $1 + 0)
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.08/x86_64-centos7-gcc48-opt/bin/root << eof
 .L /afs/cern.ch/work/r/rgargiul/ritmo/gen/hist.C
